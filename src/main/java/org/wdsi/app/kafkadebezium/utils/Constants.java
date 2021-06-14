f43dde5dc416ea9ff523d7fb26fdc7d06f7809d2
WDSI <WDSI@DESKTOP-OPE7D3E>
2021-06-14 14:16:58 +0530

    add constants

---
+++ src/main/java/org/wdsi/app/kafkadebezium/utils/Constants.java
@@ -0,0 +1,9 @@
+package org.wdsi.app.kafkadebezium.utils;
+
+public class Constants {
+
+	private Constants () {}
+	public static final String INPUT_TOPIC = "MySQlGeoTrace.kafka.geo_location";
+	
+	
+}
