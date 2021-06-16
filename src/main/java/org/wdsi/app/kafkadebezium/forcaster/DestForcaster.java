87781cc40e8a5c6f988f0599ff8e0c761768a17c
WDSI <WDSI@DESKTOP-OPE7D3E>
2021-06-16 12:12:25 +0530

    confi changes

--- src/main/java/org/wdsi/app/kafkadebezium/forcaster/DestForcaster.java
+++ src/main/java/org/wdsi/app/kafkadebezium/forcaster/DestForcaster.java
@@ -4,6 +4,7 @@ import java.util.Properties;
 
 import javax.annotation.PostConstruct;
 
+import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.common.serialization.Serdes;
 import org.apache.kafka.streams.KafkaStreams;
 import org.apache.kafka.streams.KeyValue;
@@ -61,8 +62,11 @@ public class DestForcaster {
 	
 	private Properties getProperties() {
 		Properties props = new Properties();
-		props.put(StreamsConfig.APPLICATION_ID_CONFIG, "processor1");
+		props.put(StreamsConfig.APPLICATION_ID_CONFIG, "test-stream");
 		props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+		props.setProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+		props.setProperty(StreamsConfig.PROCESSING_GUARANTEE_CONFIG, StreamsConfig.EXACTLY_ONCE); // Enable exacly once feature
+
 		props.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass());
 		props.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass());
 
