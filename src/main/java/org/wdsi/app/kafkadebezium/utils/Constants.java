6363e91fa86113989536d4e5ba01b0a26919488e
WDSI <WDSI@DESKTOP-OPE7D3E>
2021-06-16 12:05:14 +0530

    asasa

--- src/main/java/org/wdsi/app/kafkadebezium/utils/Constants.java
+++ src/main/java/org/wdsi/app/kafkadebezium/utils/Constants.java
@@ -3,7 +3,8 @@ package org.wdsi.app.kafkadebezium.utils;
 public class Constants {
 
 	private Constants () {}
-	public static final String INPUT_TOPIC = "MySQLDebJsonSchema.kafka.geo_location";
+	//public static final String INPUT_TOPIC = "MySQLDebJsonSchema.kafka.geo_location";
+	public static final String INPUT_TOPIC = "test";
 	
 	
 }
